5fc787c4a700a490ed890ce49951dc23ab8ca11b
Lucas Kolapouski <lkolapouski@gmail.com>
2026-09-07 18:47:32 -0300

    feat(DOM-02): entidade depesa parcelada

--- parte1_dart/bin/parte1_dart.dart
+++ parte1_dart/bin/parte1_dart.dart
@@ -1,11 +1,26 @@
 import 'models/despesa.dart';
+import 'models/despesa_parcelada.dart';
 
 void main(List<String> arguments) {
-  
-  Despesa despesa = Despesa(descricao: "Pizza", valor: 60, pagador: "Gabriel", categoria: "Alimentação");
-  
-  print("");
-  print("===== [1] ENTIDADE PRINCIPAL =====");
+  Despesa despesa = Despesa(
+    descricao: 'Churrasqueira',
+    valor: 300,
+    pagador: 'Henrique',
+  );
+  DespesaParcelada despesaParcelada = DespesaParcelada(
+    descricao: 'Churrasqueira',
+    valor: 300,
+    pagador: 'Henrique',
+    numeroParcelas: 3,
+    valorParcela: 100,
+  );
+
+  print('===== [1] ENTIDADE PRINCIPAL =====');
   print(despesa);
-  print("");
+  print('');
+
+  print('===== [2] HERANÇA =====');
+  print('Despesa comum -> ficha: ${despesa.ficha()}');
+  print('Despesa parcelada -> ficha: ${despesaParcelada.ficha()}');
+  print('');
 }
